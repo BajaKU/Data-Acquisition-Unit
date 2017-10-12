93e71156e4658bdd3b20e1fff9c90e6109a7a89d
William <willgraham@live.com>
2017-10-12 18:25:45 -0500

    Required libraries and comments

--- data_collection.ino
+++ data_collection.ino
@@ -1,9 +1,27 @@
+#include <Wire.h>
+extern "C" { 
+  #include "utility/twi.h"
+}
+#include <Adafruit_MMA8451.h>
+#include <Adafruit_Sensor.h>
+#include <Adafruit_L3D20_U.h>
+
+/*-------------------------------------------
+Definition of fixed values
+-------------------------------------------*/
 
 
+
+/*-------------------------------------------
+Arduino System Setup
+-------------------------------------------*/
 void setup(){
 
 }
 
+/*-------------------------------------------
+Primary Execution
+-------------------------------------------*/
 void loop(){
 
 }
